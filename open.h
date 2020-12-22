3d1aa5b179898e7603fca541153f34d63057afcc
Scout1208 <71415363+Scout1208@users.noreply.github.com>
2020-12-22 13:26:02 +0800

    Create open.h

---
+++ open.h
@@ -0,0 +1,6 @@
+#ifndef INCLUDE_H_OPEN
+#define INCLUDE_H_OPEN
+#pragma once
+#include<stdint.h>
+void open(int32_t (*mine)[16],int32_t (*number)[16],int32_t row,int32_t column,int width_input,int height_input);
+#endif
